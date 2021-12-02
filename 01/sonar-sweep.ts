b012a43a86aab9a49f644ae064eb17bf27002418
Paul Coroneos <paul@pcoroneos.com>
2021-12-02 00:50:45 -0600

    finish day one part 2

--- 01/sonar-sweep.ts
+++ 01/sonar-sweep.ts
@@ -2,12 +2,27 @@ import { data } from './sonar-sweep-data';
 
 const countDepthIncreases = (depthData: number[]) => {
   let depthIncreases = 0;
-  depthData.forEach((depth,idx) => {
-    if(depth > depthData[idx-1]) {
+  depthData.forEach((depth, idx) => {
+    if (depth > depthData[idx - 1]) {
       depthIncreases++;
-    } 
+    }
   })
   return depthIncreases;
 }
 
-console.log('increases',countDepthIncreases(data));
+const slidingWindowIncreases = (depthData: number[]) => {
+  let depthIncreases = 0;
+  let prevWindow = 0
+  let currWindow = depthData[0] + depthData[1] + depthData[2];
+
+  for (let i = 1; i < depthData.length; i++) {
+    prevWindow = currWindow;
+    currWindow = depthData[i] + (depthData[i + 1] || 0) + (depthData[i + 2] || 0);
+    if ( currWindow > prevWindow) depthIncreases++;
+  }
+
+  return depthIncreases;
+}
+
+console.log('increases', countDepthIncreases(data));
+console.log('window_increases', slidingWindowIncreases(data));
