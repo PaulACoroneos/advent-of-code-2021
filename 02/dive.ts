9214d5246744831008526844baa16a6812592923
Paul Coroneos <paul@pcoroneos.com>
2021-12-02 23:55:05 -0600

    solve day 2 part 1

--- 02/dive.ts
+++ 02/dive.ts
@@ -1,1 +1,27 @@
-const dive
+import fs from 'fs';
+
+const depthAndHorizontalCalculator = () => {
+  fs.readFile('dive-data.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    const diveData = data;
+
+    const hashTable: Record<string, number> = {};
+
+    //we are going to create an array of k/v pairs then hash
+    const diveDataArr = diveData.split('\n').map(data => {
+      const [command, qty] = data.split(' ');
+      return { command, value: parseInt(qty) }
+    });
+
+    //now hash
+    diveDataArr.forEach(({ command, value }) => {
+      if (hashTable[command]) hashTable[command] += value;
+      else hashTable[command] = value;
+    })
+
+    //calculate final total
+    console.log(hashTable['forward'] * (hashTable['down'] - hashTable['up']))
+  })
+}
+
+console.log(depthAndHorizontalCalculator());
